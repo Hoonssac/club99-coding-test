fbdad3b6ebbc3645983e18eeaebcfce8a821eb15
b1uesoda <b1uesoda@naver.com>
2024-08-04 13:08:17 +0900

    Backjoon_10816

---
+++ src/Baekjoon_10816.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Baekjoon_10816 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Integer, Integer> Count_map = new HashMap<>();
+        StringBuilder result = new StringBuilder();
+
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            int value = sc.nextInt();
+            if (!Count_map.containsKey(value)) {
+                Count_map.put(value, 1);
+            }
+            else {
+                Count_map.replace(value, Count_map.get(value) + 1);
+            }
+        }
+
+        int M = sc.nextInt();
+        for (int i = 0; i < M; i++) {
+            int input = sc.nextInt();
+            if (Count_map.containsKey(input)) {
+                result.append(Integer.toString(Count_map.get(input)) + " ");
+            }
+            else {
+                result.append("0 ");
+            }
+        }
+        
+        System.out.println(result.toString().trim());
+
+        sc.close();
+    }
+}
+
+// 개선된 코드
+// import java.util.*;
+
+// public class Baekjoon_10816 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         HashMap<Integer, Integer> countMap = new HashMap<>();
+//         StringBuilder result = new StringBuilder();
+
+//         int N = sc.nextInt();
+//         for (int i = 0; i < N; i++) {
+//             int value = sc.nextInt();
+//             countMap.put(value, countMap.getOrDefault(value, 0) + 1);
+//         }
+
+//         int M = sc.nextInt();
+//         for (int i = 0; i < M; i++) {
+//             int input = sc.nextInt();
+//             result.append(countMap.getOrDefault(input, 0)).append(" ");
+//         }
+        
+//         System.out.println(result.toString().trim());
+//         sc.close();
+//     }
+// }
